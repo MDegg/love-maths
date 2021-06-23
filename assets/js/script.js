bd46f03277027d3582ae0d8a7aae01f133fc8c54
MDegg <matt.degg@outlook.com>
2021-06-23 17:12:41 +0000

    add event listeners for click function

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You Clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You Clicked ${gameType}`);
+            
+            }
+        })
+    }
+})
+
+
+function runGame(){
+
+
+}
+
+function checkAnswer(){
+
+
+}
+
+function calculateCorrectAnswer(){
+
+
+}
+
+function incrementScore(){
+
+
+}
+
+function incrementWrongAnswer(){
+
+
+}
+
+function incrementWrongAnswer(){
+
+
+}
+
+function displayAdditionQuestion(){
+
+
+}
+
+function displayAdditionQuestion(){
+
+
+}
+
+function displaySubtractQuestion(){
+
+
+}
+
+function displayMultiplyQuestion(){
+
+
+}

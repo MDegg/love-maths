62e63d0b3e1b0a8eb1e1066ecf3da7b8cca4d4bb
MDegg <matt.degg@outlook.com>
2021-06-23 17:46:40 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,41 +7,51 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You Clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You Clicked ${gameType}`);
+                runGame(gameType);
             
             }
         })
     }
+
+    runGame("addition");
 })
 /**
  * the main game "loop", called when the script is first loaded
  * and after the user's have answer has been processed
  */
 
-function runGame(){
+function runGame(gameType) {
 
     let num1 = Math.floor(Math.random()*25) +1 ;
     let num2 =Math.floor(Math.random()*25) +1 ;
 
-}
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
 
-
-function checkAnswer(){
-
-
-}
-
-function calculateCorrectAnswer(){
-
+    } else { 
+        alert(`unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
-function incrementScore(){
+
+function checkAnswer() {
 
 
 }
 
-function incrementWrongAnswer(){
+function calculateCorrectAnswer() {
+
+
+}
+
+function incrementScore() {
+
+
+}
+
+function incrementWrongAnswer() {
 
 
 }
@@ -56,8 +66,10 @@ function displayAdditionQuestion(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+     document.getElementById('operand1').textContent = operand1;
+     document.getElementById('operand2').textContent = operand2;
+     document.getElementById('operator').textContent = "+";
 
 }
 
